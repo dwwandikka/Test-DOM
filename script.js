f681baa521bda99b87f9deea5a3d9aa276a13011
Kadek Dwi Andika <dwiandikakadek01@gmail.com>
2024-08-29 14:34:08 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("switch-btn").addEventListener("click", function() {
+  document.getElementById("jn-bias").textContent = "Bias Wrecker in BLACKPINK";
+  document.getElementById("name").innerHTML = "Jennie Kim"
+  document.getElementById("mem-image").src = "Jennie.jpg"
+  
+  document.getElementById("mem-desc").innerHTML =
+  "<b>Jennie, atau Jennie Kim</b> adalah anggota dari grup K-Pop terkenal Blackpink. <br>" +
+  "Nama aslinya adalah Jennie Kim, dia lahir pada tanggal 16 Januari 1996 di Anyang, Korea Selatan dan pernah tinggal di Selandia Baru <br>" +
+  "Jennie dikenal karena karismanya di atas panggung, serta kemampuannya dalam rap dan bernyanyi. <br>" +
+  "Selain menjadi rapper utama BLACKPINK, dia juga memiliki fashion sense yang kuat.";
+});
